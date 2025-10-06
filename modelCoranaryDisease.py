002a644d444340608bafbd408b2125cc14a4aa09
Solange <solange_3d@mail.ru>
2025-10-06 22:30:50 +0300

    I finished building the model, but it refuse to learn, so the metrics are awful either it's test or test data

--- modelCoranaryDisease.py
+++ modelCoranaryDisease.py
@@ -10,6 +10,7 @@ from collections import Counter
 from sklearn.base import RegressorMixin # we'll inherit this in our class for calling class's structure
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_absolute_error
 
 pd.options.display.max_columns = None
 raw_data = pd.read_csv("C:\\Users\\Solange\\Desktop\\Курсовая ML по определению рисков заболевания\\framingham.csv")
@@ -50,7 +51,7 @@ for x in raw_data.columns:
     mediana = df_x.median()
     raw_data = raw_data.replace(np.nan, mediana)
 
-# print(raw_data)
+print(raw_data)
 # print(Counter(raw_data["glucose"].isna())) # Counter({False: 4238})
 # print(Counter(raw_data["education"].isna())) # Counter({False: 4238})
 # print(len(raw_data))
@@ -63,39 +64,75 @@ for x in raw_data.columns:
 
 data = raw_data.copy()
 
+loss_history = []
 
 class SGDLogisticRegression(RegressorMixin):
-    def __init__(self, lr=0.01, delta_converged=1e-100, max_steps=10e4, batch_size=64):
+    def __init__(self, lr=0.01, delta_converged=1e-3, max_steps=10000, batch_size=64):
         self.lr = lr  # learning rate. The importance of moving the weight vector towards anti-gradient in SGD.
         self.delta_converged = delta_converged  # when the learning will stop.
-        self.max_steps = max_steps  # how many steps SGD can make.
-        self.batch_size = batch_size  # the size of batch in SGD.
-        self.scaler = StandardScaler()
+        self.max_steps = max_steps              # how many steps SGD can make.
+        self.batch_size = batch_size            # the size of batch in SGD.
+        self.scaler = StandardScaler()          # normalization of the features
+        # (it subtracts the mean and divides it by standard bias).
 
-        self.W = None  # the weight vector
+        self.W = None                           # the weight vector
+        self.max_accuracy = 0
 
     def fit(self, X, Y):
 
         L, F = X.shape  # L - the length of samples, F - number of features.
-        self.W = np.zeros((F, 1))  # the weight column vector.
+        self.W = np.zeros(F)  # the weight column vector.
 
         current_step = 0  # the SGD step.
-        continueFlag = True  # the condition that we haven't reached max steps.
+        continue_flag = True  # the condition that we haven't reached max steps.
 
         X_shuffled = X.copy()
         Y_shuffled = Y.copy()
-
+        # print(type(X_shuffled))
         X_shuffled = self.scaler.fit_transform(X_shuffled)  # teach the scaler on our data and transform our data.
-
-        while (current_step < self.max_steps and continueFlag):
+        # print(type(X_shuffled))
+        while (current_step < self.max_steps and continue_flag):
             # Shuffle the samples
-            indices = np.arange(0, L)  # for example  [   0    1    2 ... 4235 4236 4237]
+            indices = np.arange(0, L)   # for example  [   0    1    2 ... 4235 4236 4237]
+            # print(indices)
             np.random.shuffle(indices)  # for example now they are [ 133  950 2391 ...  725  373 3181]
-
+            # print(indices)
             X_shuffled = X_shuffled[indices]
             Y_shuffled = Y_shuffled[indices]
+            # print(X_shuffled)
+            # print(Y_shuffled)
+            # here comes a new epoch
+            for i in range(0, L, self.batch_size):
+                last_weight_vector = self.W.copy()
+                if current_step != self.max_steps:
 
-            break
+                    X_batch = X_shuffled[i: i + self.batch_size]  # create current batches
+                    Y_batch = Y_shuffled[i: i + self.batch_size]
+
+                    # calculate the sigmoid function for every row of data
+                    pred = [1/(1 + np.e**np.dot(self.W, X_batch[x])) for x in range(len(X_batch))]
+                    # calculate the gradient
+                    grad = sum([np.dot(sum(X_batch[x]), (Y_batch - pred)[x]) for x in range(len(X_batch))])
+                    # move the weight vector towards anti-gradient
+                    self.W -= (self.lr * grad)
+
+                    loss_history.append(mean_absolute_error(pred, Y_batch))
+                    if current_step%100 == 0:
+                        print(f"Training is finished on {current_step/100}% with accuracy {loss_history[current_step]}")
+                        # print(self.W)
+
+                    current_step += 1
+
+                    if np.linalg.norm(self.W - last_weight_vector) < self.delta_converged:
+                        continue_flag = False
+                        break
+                else:
+                    continue_flag = False
+                    break
+
+    def predict(self, X):
+        x_scaled = self.scaler.transform(X)
+        return [1/(1 + np.e**np.dot(self.W, x_scaled[x])) for x in range(len(x_scaled))]
 
 # print(data)
 test_size = 0.3
@@ -103,5 +140,13 @@ random_state = 42
 data_train, data_test, Y_train, Y_test = train_test_split(data.drop("TenYearCHD", axis=1),
                                                           np.array(data["TenYearCHD"]), test_size=test_size,
                                                           random_state=random_state)
-LogicRegressor = SGDLogisticRegression()
-LogicRegressor.fit(data_train, Y_train)
+LogicRegressor = SGDLogisticRegression()  # our logistic Regression
+LogicRegressor.fit(data_train, Y_train)   # train our logistic Regression
+# print(LogicRegressor.predict(data_test))
+print(mean_absolute_error(LogicRegressor.predict(data_test), Y_test))  # see the metrics
+
+# straight correlations:
+# y1 = pd.Series(data["TenYearCHD"])
+# for x in data.columns:
+#     x1 = data[x]
+#     print(f"{x} and CHD: ", y1.corr(x1))
