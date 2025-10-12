d5b6fed9c32332c62b2a37a6a4f378e67dd36da7
Solange <solange_3d@mail.ru>
2025-10-12 18:27:19 +0300

    The model is ready, but it waits some improvements, as the balanced_accuracy is quite average: 0.648

--- modelCoranaryDisease.py
+++ modelCoranaryDisease.py
@@ -10,8 +10,9 @@ from collections import Counter
 from sklearn.base import RegressorMixin # we'll inherit this in our class for calling class's structure
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import mean_absolute_error
-
+from sklearn.metrics import accuracy_score, balanced_accuracy_score
+np.random.seed(42)
+random_state = 42
 pd.options.display.max_columns = None
 raw_data = pd.read_csv("C:\\Users\\Solange\\Desktop\\Курсовая ML по определению рисков заболевания\\framingham.csv")
 
@@ -64,26 +65,24 @@ print(raw_data)
 
 data = raw_data.copy()
 
-loss_history = []
-
 class SGDLogisticRegression(RegressorMixin):
     def __init__(self, lr=0.01, delta_converged=1e-3, max_steps=10000, batch_size=64):
-        self.lr = lr  # learning rate. The importance of moving the weight vector towards anti-gradient in SGD.
+        self.lr = lr           # learning rate. The importance of moving the weight vector towards anti-gradient in SGD.
         self.delta_converged = delta_converged  # when the learning will stop.
         self.max_steps = max_steps              # how many steps SGD can make.
         self.batch_size = batch_size            # the size of batch in SGD.
         self.scaler = StandardScaler()          # normalization of the features
+        # self.threshold = threshold              # the probability when the model start to predict class 1.
         # (it subtracts the mean and divides it by standard bias).
 
         self.W = None                           # the weight vector
-        self.max_accuracy = 0
 
     def fit(self, X, Y):
 
-        L, F = X.shape  # L - the length of samples, F - number of features.
+        L, F = X.shape        # L - the length of samples, F - number of features.
         self.W = np.zeros(F)  # the weight column vector.
 
-        current_step = 0  # the SGD step.
+        current_step = 0      # the SGD step.
         continue_flag = True  # the condition that we haven't reached max steps.
 
         X_shuffled = X.copy()
@@ -110,15 +109,14 @@ class SGDLogisticRegression(RegressorMixin):
                     Y_batch = Y_shuffled[i: i + self.batch_size]
 
                     # calculate the sigmoid function for every row of data
-                    pred = [1/(1 + np.e**np.dot(self.W, X_batch[x])) for x in range(len(X_batch))]
+                    probability = [1/(1 + np.e**np.dot(self.W, X_batch[x])) for x in range(len(X_batch))]
                     # calculate the gradient
-                    grad = sum([np.dot(sum(X_batch[x]), (Y_batch - pred)[x]) for x in range(len(X_batch))])
+                    grad = sum([np.dot(sum(X_batch[x]), (Y_batch - probability)[x]) for x in range(len(X_batch))])
                     # move the weight vector towards anti-gradient
                     self.W -= (self.lr * grad)
 
-                    loss_history.append(mean_absolute_error(pred, Y_batch))
                     if current_step%100 == 0:
-                        print(f"Training is finished on {current_step/100}% with accuracy {loss_history[current_step]}")
+                        print(f"Training is finished on {current_step/100}%")
                         # print(self.W)
 
                     current_step += 1
@@ -129,22 +127,44 @@ class SGDLogisticRegression(RegressorMixin):
                 else:
                     continue_flag = False
                     break
+        return self.W
 
-    def predict(self, X):
+    def predict(self, X, threshold=0.5):
         x_scaled = self.scaler.transform(X)
-        return [1/(1 + np.e**np.dot(self.W, x_scaled[x])) for x in range(len(x_scaled))]
+        probabilities = [1/(1 + np.e**np.dot(self.W, x_scaled[x])) for x in range(len(x_scaled))]
+        return [1 if i > threshold else 0 for i in probabilities]
 
 # print(data)
 test_size = 0.3
-random_state = 42
+
+
 data_train, data_test, Y_train, Y_test = train_test_split(data.drop("TenYearCHD", axis=1),
                                                           np.array(data["TenYearCHD"]), test_size=test_size,
                                                           random_state=random_state)
 LogicRegressor = SGDLogisticRegression()  # our logistic Regression
-LogicRegressor.fit(data_train, Y_train)   # train our logistic Regression
+weights_of_the_model = LogicRegressor.fit(data_train, Y_train)   # train our logistic Regression
 # print(LogicRegressor.predict(data_test))
-print(mean_absolute_error(LogicRegressor.predict(data_test), Y_test))  # see the metrics
+# thresholds = [x/100 for x in range(1, 100)]
+thresholds = np.arange(0.01, 1, step=0.01)
+best_threshold = 0
+best_result = 0
+# result = balanced_accuracy_score(Y_test, LogicRegressor.predict(data_test, threshold=0.5))  # see the metrics
+# print(result)
+for x in thresholds:
+    result = balanced_accuracy_score(Y_test, LogicRegressor.predict(data_test, threshold=x))  # see the metrics
+    if result > best_result:
+        best_result = result
+        best_threshold = x
 
+print(f"The best result is {best_result} using threshold {best_threshold}") # balanced_accuracy_score = 0.648
+
+# the best threshold is 0.45
+
+result = balanced_accuracy_score(Y_test, LogicRegressor.predict(data_test, threshold=0.45))
+print("Balanced_accuracy_score:", result)
+
+# print(balanced_accuracy_score(Y_test, LogicRegressor.predict(data_test, threshold=0.66, W=weights_of_the_model)))
+# print(weights_of_the_model)
 # straight correlations:
 # y1 = pd.Series(data["TenYearCHD"])
 # for x in data.columns:
