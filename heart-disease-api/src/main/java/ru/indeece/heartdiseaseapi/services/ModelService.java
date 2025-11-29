4de005993434aa5cbaf04369c2f487c8ae3f617c
Indeece <anton.shkonda@yandex.ru>
2025-11-29 13:45:18 +0300

    Improve risk calculation precision

--- heart-disease-api/src/main/java/ru/indeece/heartdiseaseapi/services/ModelService.java
+++ heart-disease-api/src/main/java/ru/indeece/heartdiseaseapi/services/ModelService.java
@@ -5,20 +5,18 @@ import org.springframework.stereotype.Service;
 import ru.indeece.heartdiseaseapi.apiclient.HeartDiseaseApiClient;
 import ru.indeece.heartdiseaseapi.dto.RiskRequest;
 import ru.indeece.heartdiseaseapi.dto.RiskResponse;
-import ru.indeece.heartdiseaseapi.properties.ModelProperties;
 
 
 @Service
 @RequiredArgsConstructor
 public class ModelService {
 
-    private final ModelProperties modelProperties;
     private final HeartDiseaseApiClient apiClient;
 
 
     public double analyzeRisk(RiskRequest riskRequest) {
         RiskResponse response = apiClient.getAnalyzedRisk(riskRequest);
         double risk = response.getDiseaseRisk();
-        return Math.round(risk);
+        return Math.round(risk * 100.0) / 100.0;
     }
 }
