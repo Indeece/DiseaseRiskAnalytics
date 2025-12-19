89182d01aa9a818d92ad753c69f3f01c814297d8
SvetoslavSO <sshaunin@mail.ru>
2025-12-19 22:15:10 +0300

    add cors configuration

--- auth-service/src/main/java/com/svet/authservice/security/SecurityConfig.java
+++ auth-service/src/main/java/com/svet/authservice/security/SecurityConfig.java
@@ -15,6 +15,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -34,8 +39,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf((csrf) -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Включаем CORS с конфигурацией
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/login", "auth/register", "/auth/signIn", "/auth/refresh").permitAll()
+                        .requestMatchers("/auth/login", "/auth/register", "/auth/signIn", "/auth/refresh").permitAll()
                         .requestMatchers("/auth/user/**").hasAuthority("ROLE_USER")
                         .anyRequest().authenticated()
                 )
@@ -43,6 +49,19 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // Разрешаем фронтенд
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Разрешаем методы
+        configuration.setAllowedHeaders(Arrays.asList("*")); // Разрешаем все заголовки
+        configuration.setAllowCredentials(true); // Разрешаем отправку куки/авторизации
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Применяем ко всем путям
+        return source;
     }
 
     @Bean
