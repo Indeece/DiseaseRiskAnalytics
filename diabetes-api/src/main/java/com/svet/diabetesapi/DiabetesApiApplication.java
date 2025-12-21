add423721254df9d2faa97370449551391f7a06d
Indeece <anton.shkonda@yandex.ru>
2025-12-21 13:54:49 +0300

    Add eureka annotation

--- diabetes-api/src/main/java/com/svet/diabetesapi/DiabetesApiApplication.java
+++ diabetes-api/src/main/java/com/svet/diabetesapi/DiabetesApiApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 @EnableConfigurationProperties
+
 public class DiabetesApiApplication {
 
     public static void main(String[] args) {
