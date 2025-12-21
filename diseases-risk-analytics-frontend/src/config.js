baf4f715d391b2c7c3d621f49005e3ef3eeb7f9d
SvetoslavSO <sshaunin@mail.ru>
2025-12-21 15:19:35 +0300

    Refactor: add docker compose and change nginx.conf

--- diseases-risk-analytics-frontend/src/config.js
+++ diseases-risk-analytics-frontend/src/config.js
@@ -1,7 +1,7 @@
 const API_CONFIG = {
   // Для разработки - локальный gateway
   // Для продакшена в Docker - будет заменено через переменные окружения
-  BASE_URL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8090',
+  BASE_URL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000',
   
   
   PATHS: {
